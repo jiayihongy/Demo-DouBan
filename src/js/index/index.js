2df4b8f2c6aa5d78120559e27510fe6c5b9cc444
haoz <z8817176@163.com>
2017-09-12 23:21:30 +0800

    first upload

---
+++ src/js/index/index.js
@@ -0,0 +1,10 @@
+define(["jquery", "template"], function($, template) {
+    require(['muban'], function(dada) {
+        console.log(dada)
+    });
+
+    $('.scroll-arr a').on('click', function() {
+
+    })
+
+});
